3d94fa8b04acca9d59fe05470d919a2dac22755f
Nasvishchuk Olena <o.nasvishchuk@gmail.com>
2023-02-02 21:17:06 +0000

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,47 +1,40 @@
 
-const mainDivEl = document.querySelector('#controls');
-const onInputChangeEl = mainDivEl.firstElementChild;
-const createBtnEl = document.querySelector('button[data-create]');
-const DestroyBtnEl = document.querySelector('button[data-destroy]');
-const containerForBoxes = document.querySelector("#boxes");
-
-DestroyBtnEl.addEventListener('click', () => { 
-  onInputChangeEl.value = 0;
-})
-
-createBtnEl.addEventListener('click', createBoxes);
-
-
-function createBoxes(amount) { 
-  amount = onInputChangeEl.value;
-  const boxesArray = [];
-  const randomColor = getRandomHexColor();
-  amount.map((item, index) => {
-    const newBox = document.createElement("div");
-    newBox.style.backgroumdColor = randomColor;
-    newBox.style.width = 30 + index * 10 + "px";
-    newBox.style.height = 30 + index * 10 + "px";
-    boxesArray.push(item);
-    containerForBoxes.append(...boxesArray);
-    return containerForBoxes;
-})
-}
-
-
-createBtnEl.removeEventListener('click', createBoxes);
-
-
-
-
-
-
-
-
-
-
-
-
+const onInputChangeEl = document.querySelector('#controls>input[type="number"]')
+const createBtnEl = document.querySelector('button[data-create]')
+const destroyBtnEl = document.querySelector('button[data-destroy]')
+const containerForBoxes = document.querySelector('#boxes')
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+createBtnEl.addEventListener('click', handlerOnClick)
+
+function handlerOnClick(event) {
+  const amount = onInputChangeEl.value
+  console.log('amount', amount)
+  createBoxes(amount)
+}
+
+
+const createBoxes = (amount) => {
+  const boxesArray = []
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement('div')
+    newBox.style.backgroundColor = getRandomHexColor()
+    newBox.style.width = `${30 + i * 10}px`
+    newBox.style.height = `${30 + i * 10}px`
+    boxesArray.push(newBox)
+    containerForBoxes.append(...boxesArray)
+  }
+}
+
+
+destroyBtnEl.addEventListener('click', () => {
+    onInputChangeEl.value = 0
+    containerForBoxes.innerHTML=''
+})
+
+
+
+
